361a6d29ce2f63da25f86f23356ecd434745ff32
Трефилов Дмитрий Александрович <DATrefilov@ibs.root.ibscorp.ru>
2022-04-09 22:08:50 +0500

    Выполнил задание 8.14. Правда или действие (часть 2)

--- js/openBigPhoto.js
+++ js/openBigPhoto.js
@@ -4,11 +4,15 @@ const closePhotoModal = document.querySelector('.big-picture__cancel');
 const commentTemplate = cardPhotoModal.querySelector('.social__comment');
 const likesCount = cardPhotoModal.querySelector('.likes-count');
 const commentsCount = cardPhotoModal.querySelector('.comments-count');
+const commentsLoader = document.querySelector('.social__comments-loader');
+const commentsList = cardPhotoModal.querySelector('.social__comments');
+let postComments = [];
+let lastFilledComments = 0;
 
-function fillCommentsBlock (comments) {
-  const commentsList = cardPhotoModal.querySelector('.social__comments');
-  commentsList.innerHTML = '';
-  comments.forEach((commentData) => {
+function fillCommentsBlock () {
+  const partedComments = postComments.slice(lastFilledComments,lastFilledComments+5);
+  const filledComments = document.querySelector('.comments-filled');
+  partedComments.forEach((commentData) => {
     const comment = commentTemplate.cloneNode(true);
     const commentImg = comment.querySelector('img');
     commentImg.src = commentData.avatar;
@@ -16,13 +20,26 @@ function fillCommentsBlock (comments) {
     comment.querySelector('.social__text').textContent = commentData.message;
     commentsList.append(comment);
   });
+  lastFilledComments += partedComments.length;
+  if (lastFilledComments >= postComments.length) {
+    commentsLoader.classList.add('hidden');
+  }
+  filledComments.textContent = lastFilledComments;
+}
+
+function resetBigPicture() {
+  commentsLoader.classList.remove('hidden');
+  lastFilledComments = 0;
 }
 
 function fillPhotoCardModal (card) {
+  resetBigPicture();
   cardPhotoModalImage.src = card.url;
   likesCount.textContent = card.likes;
+  postComments = card.comments;
   commentsCount.textContent = card.comments.length.toString();
-  fillCommentsBlock(card.comments);
+  commentsList.innerHTML = '';
+  fillCommentsBlock();
   cardPhotoModal.classList.remove('hidden');
   closePhotoModal.addEventListener('click', () =>{
     cardPhotoModal.classList.add('hidden');
@@ -34,6 +51,7 @@ function fillPhotoCardModal (card) {
       document.body.classList.remove('modal-open');
     }
   });
+  commentsLoader.addEventListener('click', fillCommentsBlock);
 }
 
 function createBigPhotoModalFrame(cards) {
@@ -42,8 +60,6 @@ function createBigPhotoModalFrame(cards) {
     photoCard[i].addEventListener('click', (evt) => {
       const cardId = evt.target.closest('.picture').dataset.cardId;
       fillPhotoCardModal(cards[cardId]);
-      cardPhotoModal.querySelector('.social__comment-count').classList.add('hidden');
-      cardPhotoModal.querySelector('.comments-loader').classList.add('hidden');
       document.body.classList.add('modal-open');
     });
   }
